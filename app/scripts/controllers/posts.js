a9234e6043fec5489b6bfbec771fc42f44bd7fb5
rszczypka <rszczypka@gmail.com>
2014-04-28 21:41:07 +0100

    Adding voting to our application
    Let's make the functionality for upvoting and downvoting posts.

--- app/scripts/controllers/posts.js
+++ app/scripts/controllers/posts.js
@@ -1,14 +1,40 @@
 'use strict';
 
 app
-.controller('PostsCtrl', function ($scope, $location, Post) {
-	if($location.path() === '/'){
-		$scope.posts = Post.all;
-	}
-	
-	$scope.post = {url: 'http://'};
+    .controller('PostsCtrl', function($scope, $location, Post) {
+        if ($location.path() === '/') {
+            $scope.posts = Post.all;
+        }
 
-	$scope.deletePost = function (postId) {
-    	Post.delete(postId);
-    };
-});
+        $scope.post = {
+            url: 'http://'
+        };
+
+        $scope.deletePost = function(postId) {
+            Post.delete(postId);
+        };
+
+        $scope.upVotePost = function(postId, upVoted) {
+            if (upVoted) {
+                Post.clearVote(postId, upVoted);
+            } else {
+                Post.upVote(postId);
+            }
+        };
+
+        $scope.downVotePost = function(postId, downVoted) {
+            if (downVoted) {
+                Post.clearVote(postId, !downVoted);
+            } else {
+                Post.downVote(postId);
+            }
+        };
+
+        $scope.upVoted = function(post) {
+            return Post.upVoted(post);
+        };
+
+        $scope.downVoted = function(post) {
+            return Post.downVoted(post);
+        };
+    });
